c8c16fd79fd2e7145777326cfd25daafc890f909
Eachen <695089605@qq.com>
2018-12-05 15:25:22 +0800

    update Chapter 2

--- functionsal_tests.py
+++ functionsal_tests.py
@@ -1,4 +1,21 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+class NewVisitorTest(unittest.TestCase): #➊
+
+	def setUp(self): #➋
+		self.browser = webdriver.Chrome()
+
+	def tearDown(self): #➌
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self): #➍
+		# 伊迪丝听说有一个很酷的在线待办事项应用
+		# 她去看了这个应用的首页
+		self.browser.get('http://localhost:8000')
+		# 她注意到网页的标题和头部都包含“To-Do”这个词
+		self.assertIn('To-Do', self.browser.title) #➎
+		self.fail('Finish the test!') #➏
+		# 应用邀请她输入一个待办事项
+
+if __name__ == '__main__': #➐
+	unittest.main(warnings='ignore') #➑
