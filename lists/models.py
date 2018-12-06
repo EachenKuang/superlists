23736611430829b95ec5dfbe9d0174ea9a69a328
Eachen <695089605@qq.com>
2018-12-06 21:29:46 +0800

    update

--- lists/models.py
+++ lists/models.py
@@ -7,7 +7,7 @@ class List(models.Model):
 
 class Item(models.Model):
 	text = models.TextField(default="")
-	list = models.ForeignKey(List, on_delete=models.DO_NOTHING)
+	list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
 
 
