4666d7fc6e437da018fa064ee8bd12c77b905a00
Eachen <695089605@qq.com>
2018-12-06 16:03:56 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -62,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 我们使用一个新浏览器会话
         # 确保伊迪丝的信息不会从cookie中泄露出来
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
         # 弗朗西斯访问首页
         # 页面中看不到伊迪丝的清单
